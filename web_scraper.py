d612411afa8b46d11db875542a6545d16aa4008e
kba0297 <kba0297@stacmail.net>
2024-03-02 17:03:03 +1300

    Finding lowest price of game
    - Also covering edge cases with if statements

--- web_scraper.py
+++ web_scraper.py
@@ -12,11 +12,31 @@ soup = BeautifulSoup(page.content, "html.parser")
 name_element = soup.find(id="appHubAppName")
 price_element = soup.find(id="game_area_purchase")
 
-price_element_1 = price_element.find_all(class_="discount_final_price")
-for price in price_element_1:
-    print(price.text.strip('$'))
-    #make each individual value an integer and add to a different list
-    #then find lowest value of list and assign to variable
+discount_prices = []
+normal_prices = []
 
+if price_element.find(class_="discount_final_price"):
+    discount_prices = price_element.find_all(class_="discount_final_price")
+if price_element.find(class_="game_purchase_price price"):
+    normal_prices = price_element.find_all(class_="game_purchase_price price")
 
+all_prices = discount_prices + normal_prices
+price_list = []
+
+for price in all_prices:
+    print(price.text.strip('NZ$ '))
+    price_1 = float(price.text.strip('\r\n\t\t\t\t\t\t\tNZ$ \t\t\t\t\t\t'))
+    price_list.append(price_1)
+
+price_list.sort()
+
+if soup.find(id="dlc_purchase_action"):
+    dlc_element = soup.find(id="dlc_purchase_action")
+    if dlc_element.find(class_="game_purchase_price price"):
+        dlc_price = dlc_element.find(class_="game_purchase_price price")
+        dlc_price_1 = float(dlc_price.text.strip('\r\n\t\t\t\t\t\t\t\t\t\t\t\t\tNZ$ \t\t\t\t\t\t\t\t\t\t\t\t'))
+        if price_list[0] == dlc_price_1:
+            del price_list[0]
+
+print(price_list[0])
 print(name_element.text.strip())
