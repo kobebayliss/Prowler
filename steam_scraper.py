e7c15356ba2c3c3031fc1962c8375bab74ad16b9
kba0297 <kba0297@stacmail.net>
2024-03-08 14:00:52 +1300

    Find genre, developer
    - Added features to find genre
    - Find developer

--- steam_scraper.py
+++ steam_scraper.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
+
+games_names = []
 
 #function to stop errors if a certain class doesn't exist
 def error_handle(container_1, class_name, variable):
@@ -11,14 +14,38 @@ def error_handle(container_1, class_name, variable):
         variable = f"Failed to find {variable}"
     return variable
 
+steam_url = "https://store.steampowered.com/search/?sort_by=_ASC&filter=topsellers&os=win&supportedlang=english"
+
+#initialise web driver
+driver = webdriver.Chrome()
+driver.get(steam_url)
+
 def scrape_steam_page(url):
     #loading page
     response = requests.get(url)
 
     #checking that the page successfully loaded
     if response.status_code == 200:
+        #time between each scroll
+        scroll_pause_time = 0.4
+        #find screen height
+        screen_height = driver.execute_script("return window.screen.height;")
+        i = 1
+        #while loop to continue scrolling
+        while True:
+            #scroll down
+            driver.execute_script(f"window.scrollTo(0, {screen_height * i});")
+            i += 1
+            time.sleep(scroll_pause_time)
+            driver.execute_script("return document.body.scrollHeight;")
+            #if condition for when website should finish scrolling
+            if i > 5:
+                break
+        
+        updated_html = driver.page_source
+
         #parsing website's HTML
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(updated_html, 'html.parser')
 
         #creating a container for all the content
         container = soup.find(id="search_resultsRows")
@@ -40,6 +67,12 @@ def scrape_steam_page(url):
             soup2 = BeautifulSoup(game_page.content, 'html.parser')
             #error handling and retrieving description
             description = error_handle(soup2, "game_description_snippet", "description")
+            if soup2.find(id="genresAndManufacturer"):
+                genre_container = soup2.find(id="genresAndManufacturer")
+                genre = genre_container.find('span').text.strip()
+                developer = genre_container.find('div', class_="dev_row", 'a').text.strip()
+            else:
+                genre = "Failed to find genre"
 
             #print out all information about the game
             print(f"Game {index}:")
@@ -47,11 +80,15 @@ def scrape_steam_page(url):
             print(f"Price: {price}")
             print(f"Link: {link}")
             print(f"Description: {description}")
+            print(f"Genre: {genre}")
+            print(f"Developer: {developer}")
             print()
             index+=1
+            games_names.append(name)
+        driver.quit()
 
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
-steam_url = "https://store.steampowered.com/search/?sort_by=_ASC&filter=topsellers&os=win&supportedlang=english"
 games_list = scrape_steam_page(steam_url)
+print(games_names)
