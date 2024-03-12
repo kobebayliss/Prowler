4ea8558b6e0091c07cb600f4df808257391244ea
kba0297 <kba0297@stacmail.net>
2024-03-12 13:35:33 +1300

    Working on epic scraper
    - Made the scraper undetectable so don't get cloudflare checked
    - Able to get every game's name on epic games by iterating through the pages

--- epic_scraper.py
+++ epic_scraper.py
@@ -3,21 +3,32 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from fuzzywuzzy import fuzz
+import undetected_chromedriver as uc 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options 
 import time
 
-epic_url = "https://store.epicgames.com/en-US/browse?sortBy=title&sortDir=ASC&category=Game&count=100"
+options = uc.ChromeOptions() 
+options.headless = False
+driver = uc.Chrome(options=options) 
 
-driver = webdriver.Chrome()
-driver.get(epic_url)
+def scrape_epic_page():
+    start_value = 0
+    while start_value < 3800:
+        epic_url = f"https://store.epicgames.com/en-US/browse?sortBy=title&sortDir=ASC&category=Game&count=100&start={start_value}"
+        driver.get(epic_url)
+        time.sleep(2)
+        updated_html = driver.page_source
+        soup = BeautifulSoup(updated_html, 'html.parser')
+        container = soup.find('ul', class_="css-cnqlhg")
+        for game in container.find_all('li'):
+            if game.find('div', class_="css-rgqwpc"):
+                name = game.find('div', class_="css-rgqwpc").text.strip()
+            else:
+                name = game.find('div', class_="css-8uhtka").text.strip()
+            print(name)
+        start_value += 100
 
-def scrape_epic_page(url):
-    updated_html = driver.page_source
-    soup = BeautifulSoup(updated_html, 'html.parser')
-
-    container = soup.find('ul', class_="css-cnqlhg")
-    for game in container.find_all('li'):
-        name = game.find('div', class_="css-rgqwpc")
-        print(name)
-
-epic_scraper = scrape_epic_page(epic_url)
+epic_scraper = scrape_epic_page()
 print(epic_scraper)
