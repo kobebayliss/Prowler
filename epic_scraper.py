62605b60dfe9159c8ae2a3b1b1fa4eec2c54e7e0
kba0297 <kba0297@stacmail.net>
2024-03-13 13:09:38 +1300

    Epic Scraper Prices
    - Added the ability to scrape prices
    - Made it so I can control the max pages scraped to improve the speed of my testing

--- epic_scraper.py
+++ epic_scraper.py
@@ -15,7 +15,8 @@ driver = uc.Chrome(options=options)
 
 def scrape_epic_page():
     start_value = 0
-    while start_value < 3800:
+    max_games = int(input("Enter number of games : "))
+    while start_value < max_games:
         epic_url = f"https://store.epicgames.com/en-US/browse?sortBy=title&sortDir=ASC&category=Game&count=100&start={start_value}"
         driver.get(epic_url)
         time.sleep(2)
@@ -27,7 +28,13 @@ def scrape_epic_page():
                 name = game.find('div', class_="css-rgqwpc").text.strip()
             else:
                 name = game.find('div', class_="css-8uhtka").text.strip()
-            print(name)
+            if game.find('div', class_="css-l24hbj"):
+                price_container = game.find('div', class_="css-1a6kj04")
+                price_container_2 = price_container.find('div', class_="css-o1hbmr")
+                price = price_container_2.find('span', class_="css-119zqif").text.strip()
+            else:
+                price = "N/A"
+            print(f"{name.ljust(60)} - {price.rjust(15)}")
         start_value += 100
 
 epic_scraper = scrape_epic_page()
