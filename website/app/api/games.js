85a66aac7dead4f90d87092ac5d938b4d85ffe2e
kba0297 <kba0297@stacmail.net>
2024-06-17 10:08:31 +1200

    New file format in order to hopefully deploy backend also

--- website/app/api/games.js
+++ website/app/api/games.js
@@ -1,11 +1,25 @@
-import db from "../config/db";
+import { Client } from 'pg';
 
-export default function handler(req, res) {
-    const sql = "SELECT * FROM games";
-    db.query(sql)
-        .then(data => res.json(data.rows))
-        .catch(err => {
-            console.error("Error ", err);
-            res.status(500).json({ error: "Server Error" });
-        });
+export default async function handler(req, res) {
+    const db = new Client({
+        user: 'default',
+        host: 'ep-hidden-night-a7qy5ttw-pooler.ap-southeast-2.aws.neon.tech',
+        database: 'verceldb',
+        password: '7vnKeSWk3yog',
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+
+    try {
+        await db.connect();
+        const result = await db.query("SELECT * FROM games");
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error('Database connection failed:', err.stack);
+        res.status(500).json({ error: 'Server Error' });
+    } finally {
+        await db.end();
+    }
 }
